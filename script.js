8ca27f7a6a1b43053b2ee80d4bb4ce2214bc898e
Midhat-hub <midhatsayyed14@gmail.com>
2025-01-31 12:11:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const facts = [
-    "I love hiking.",
-    "I am an avid coder and love to code often",
-    "I once cooked a Taco",
-    "I speak three languages fluentlys.",
-    "I’m learning to play the guitar in my free time."
+  "Cats spend between 30 to 50 percent of their day grooming themselves"
+"Purring doesn't always mean a cat is happy. ..."
+"It's possible that purring helps bone density. ..."
+"A cat's nose has catnip receptors. ..."
+"But most cats don't respond to catnip. ..."
+"Cats make great private detectives."
 ];
 
 function generateFact() {
